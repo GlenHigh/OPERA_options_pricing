cb6f593bd21b5a89ef5f48ead982c226e25196dd
glen <glen.high06@gmail.com>
2021-11-26 01:34:57 +0100

    root dash (+ yahoo inverted quote pb?... to see...)

---
+++ README.md
@@ -0,0 +1,35 @@
+# Web scraping options pricer
+
+## Web scraping
+
+Using web scraping algos to get market data (Ticker/K/T otpion quotes, spot, dividends, spot/options volumes, ...).</br>
+Source is [Yahoo finance](yahoo.finance.com).</br></br>
+Scraping tech relies on selenium, beautifulsoup and chromedriver.</br>
+
+## Dahboarding
+
+Interactive dashboard enables user to plot prices and vol surface (3D surface to come)</br>
+A pricing interface will be added soon.</br></br>
+Dashboarding is achieved via plotly's dash library.
+
+## Quant library
+
+Inhouse quant lib with handy data-handling</br></br>
+Vol surface construction using:</br>
+- Polynomial regression
+- Natural cubic splines
+- Smoothing cubic splines
+- Blend of smoothed observations (using smoothing splines) and polynomial regression (to come soon)
+</br></br>
+Pricing library will house (in progress):
+- Local volatility under Dupire's framework (vol or price version, too see)
+- LSV model 
+- Hard-coded deep learning lib (in progress)
+- Hard-coded optimizers (to come soon)
+- Blending statistical model (could range anywhere from basic stuff up to deepL)
+- Blended vol model (LV + (or *) adjustment (or scaling) factor to mimick LV-LSV pricing error)
+
+## Fake order-placing (to come)
+Illustrative module faking options order-placing from the click of dash buttons</br>
+Based on CBOE's FIX protocol for US options : [CBOE FIX](https://cdn.cboe.com/resources/membership/US_Options_FIX_Specification.pdf)</br>
+
--- dash/app.py
+++ dash/app.py
@@ -1,23 +1,72 @@
 import dash
+from dash.dependencies import Input, Output
 import dash_core_components as dcc
 import dash_html_components as html
+import pandas as pd
 
 app = dash.Dash(__name__)
 
+c_data = pd.read_csv('/Users/glenhigh/Scrapping/OPERA_options_pricing/Market_data/Calls_AAPL_Last.csv')
+p_data = pd.read_csv('/Users/glenhigh/Scrapping/OPERA_options_pricing/Market_data/Puts_AAPL_Last.csv')
+
+
+
 app.layout = html.Div([
+    html.H1("Vol K T visualization"),
+    html.P("Zone test"),
     dcc.Dropdown(
+        id="ticker",
         options=[
             {'label': 'Apple AAPL', 'value': 'AAPL'},
             {'label': 'Microsoft MSFT', 'value': 'MTL'},
             {'label': 'CAC40 FCHI', 'value': '^FCHI'}
         ],
         value='AAPL'
+    ),
+    dcc.Dropdown(
+        id="dates",
+        options=[
+            {'label':date, 'value':date}
+            for date in set(c_data['Date'])
+        ],
+        value=c_data['Date'][0]
+    ),
+    dcc.Graph(
+        id="price", config={"displayModeBar": False},
     )
 ])
 
-c_data = pd.read_csv('/Users/glenhigh/Scrapping/OPERA_options_pricing/Market_data/Calls_AAPL_Last.csv')
-p_data = pd.read_csv('/Users/glenhigh/Scrapping/OPERA_options_pricing/Market_data/Puts_AAPL_Last.csv')
-
+@app.callback(
+    Output("price","figure"),
+    [
+        Input("dates","value"),
+        Input("ticker","value")
+    ],
+)
+def update_price(date,ticker):
+    mask = (c_data["Date"] == date)
+    filtered=c_data.loc[mask,:]
+    x={
+        "data": [
+            {
+                "x": filtered["Strike"],
+                "y": filtered["Last Price"],
+                "type": "lines",
+                "hovertemplate": "$%{y:.2f}<extra></extra>",
+            },
+        ],
+        "layout": {
+            "title": {
+                "text": f"Price of {ticker} on {date}",
+                "x": 0.05,
+                "xanchor": "left",
+            },
+            "xaxis": {"fixedrange": True},
+            "yaxis": {"tickprefix": "$", "fixedrange": True},
+            "colorway": ["#17B897"],
+        },
+    }
+    return x
 
 if __name__ == '__main__':
     app.run_server(debug=True)
